6df110381562f6dba1cf2559d59e74333cb0a9ca
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2021-09-04 19:38:11 -0300

    refactor: :truck: App agora tem uma pasta dedicada e index.css foi transferido para app.css

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 //import { Login } from "./pages/Login/Login";
-import { Adm } from "./pages/Adm/Adm";
+import { Adm } from "../pages/Adm/Adm";
 
 function App(props) {
     //return <Login></Login>;
