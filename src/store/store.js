2d3cabf1e1e6cf50705576e2e7a2a6b6c9b88223
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2021-09-20 22:17:02 -0300

    fix: :bug: Ajuste no redux dev tools

--- src/store/store.js
+++ src/store/store.js
@@ -1,13 +1,12 @@
 import { createStore, combineReducers } from "redux";
-import devToolsEnhancer from "remote-redux-devtools";
 import userReducer from "./reducers/user.reducer";
 
 const reducers = combineReducers({
-  user: userReducer,
+    user: userReducer,
 });
 
 function storeConfig() {
-  return createStore(reducers, devToolsEnhancer());
+    return createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 }
 
 export default storeConfig;
