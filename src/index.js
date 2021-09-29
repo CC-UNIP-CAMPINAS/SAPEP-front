00094f5615064c3ac35dc25bfd7dadac86e3ec90
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2021-09-29 20:10:32 -0300

    feat: :sparkles: Implementação do toastify

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { ToastContainer } from "react-toastify";
 import "./global.scss";
+import "react-toastify/dist/ReactToastify.css";
 import AppRoutes from "./routes/routes";
 
 ReactDOM.render(
     <React.StrictMode>
         <AppRoutes />
+        <ToastContainer autoClose={3000} theme="colored" />
     </React.StrictMode>,
     document.getElementById("root")
 );
---
+++ src/services/types.js
@@ -0,0 +1,11 @@
+const types = {
+    INFO: "INFO",
+    WARNING: "WARNING",
+    ERROR: "ERROR",
+    SUCCESS: "SUCCESS",
+    NOT_FOUND: "NOT_FOUND",
+    FIELD_EMPTY: "FIELD_EMPTY",
+    NOT_AUTHORIZED: "NOT_AUTHORIZED",
+};
+
+export default types;
