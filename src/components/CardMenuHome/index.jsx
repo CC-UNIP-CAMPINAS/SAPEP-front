90f10612af3470f69ca855c95b9ba78234af8147
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2021-10-01 20:02:41 -0300

    feat(Component): :sparkles: Criando cards de menu da home

---
+++ src/components/CardMenuHome/index.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import "./styles.scoped.scss";
+import { Icon } from "@iconify/react";
+import Button from "../Button/Button";
+
+function CardMenuHome({ title, icon, quant }) {
+    return (
+        <div className="container">
+            <h1>
+                <Icon icon={icon} inline={true} /> {title}
+            </h1>
+            <h2>{quant} Ativos</h2>
+            <Button text="Gerenciar" color="cyan" />
+        </div>
+    );
+}
+
+export default CardMenuHome;
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,7 +1,25 @@
+import CardMenuHome from "../../components/CardMenuHome";
 import "./styles.scoped.scss";
 
 function Home() {
-    return <div className="container">Olá mundo</div>;
+    return (
+        <div className="container">
+            <section id="cards">
+                <span>
+                    <CardMenuHome title="Médicos" icon="vaadin:doctor" quant={100} />
+                </span>
+                <span>
+                    <CardMenuHome title="Enfermeiros" icon="wpf:medical-doctor" quant={100} />
+                </span>
+                <span>
+                    <CardMenuHome title="Pacientes" icon="fluent:doctor-48-filled" quant={100} />
+                </span>
+            </section>
+            <section id="graph">
+                <h1>Oi</h1>
+            </section>
+        </div>
+    );
 }
 
 export default Home;
