3441d6133e0b217ddb0bfda856e6190e54bb0e83
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2021-10-14 14:35:05 -0300

    refactor(Page: Home): :recycle: Refatorando home pra dar responsabilidade para os componentes internos

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,22 +1,15 @@
-import CardMenuHome from "../../components/CardMenuHome";
-import TableDoctor from "../../components/TableDoctor";
-import "./styles.scoped.scss";
 import React from "react";
-import { api } from "../../services/api";
-import { setDoctors } from "../../store/actions/doctor.action";
 import { connect } from "react-redux";
+import CardMenuHome from "../../components/CardMenuHome";
+import TableNurse from "../../components/TableNurse";
+import { api } from "../../services/api";
 import { notification } from "../../services/toastify";
 import types from "../../services/types";
-import Button from "../../components/Button/Button";
-import CardAddDoctor from "../../components/CardAddDoctor";
-import Popup from "reactjs-popup";
+import { setDoctors } from "../../store/actions/doctor.action";
+import { setNurses } from "../../store/actions/nurse.action";
+import "./styles.scoped.scss";
 
-function Home({ populateDoctors, doctors }) {
-    const [isCheck, setIsCheck] = React.useState(true);
-    const modalRef = React.useRef();
-    const openModal = () => modalRef.current.open();
-    const closeModal = () => modalRef.current.close();
-
+function Home({ populateDoctors, doctors, populateNurses, nurses }) {
     React.useEffect(() => {
         async function loadDoctors() {
             try {
@@ -27,16 +20,20 @@ function Home({ populateDoctors, doctors }) {
                 notification(types.ERROR, error.message);
             }
         }
-        loadDoctors();
-    }, [populateDoctors]);
 
-    function handleShowDoctor() {
-        if (!isCheck) {
-            return doctors;
-        } else {
-            return doctors.filter((doctor) => doctor.active);
+        async function loadNurses() {
+            try {
+                const { data } = await api.get("/user/nurse");
+                populateNurses(data);
+            } catch (error) {
+                console.log(error);
+                notification(types.ERROR, error.message);
+            }
         }
-    }
+        
+        loadDoctors();
+        loadNurses();
+    }, [populateDoctors, populateNurses]);
 
     return (
         <div className="container">
@@ -45,7 +42,7 @@ function Home({ populateDoctors, doctors }) {
                     <CardMenuHome title="Médicos" icon="vaadin:doctor" quant={doctors.length} active />
                 </span>
                 <span>
-                    <CardMenuHome title="Enfermeiros" icon="wpf:medical-doctor" quant={100} />
+                    <CardMenuHome title="Enfermeiros" icon="wpf:medical-doctor" quant={nurses.length} />
                 </span>
                 <span>
                     <CardMenuHome title="Administração" icon="wpf:administrator" quant={100} />
@@ -54,19 +51,8 @@ function Home({ populateDoctors, doctors }) {
                     <CardMenuHome title="Pacientes" icon="fluent:doctor-48-filled" quant={100} />
                 </span>
             </section>
-            <section id="buttons">
-                <span>
-                    <Button text="Adicionar médico" color="cyan" handle={openModal} />
-                </span>
-                <div />
-                <p>Somente médicos ativos</p>
-                <input type="checkbox" checked={isCheck} onChange={() => setIsCheck(!isCheck)} />
-                <Popup ref={modalRef} modal>
-                    <CardAddDoctor close={closeModal} />
-                </Popup>
-            </section>
             <section id="table">
-                <TableDoctor header={["Id", "Nome", "Email", "CRM", "Área", "Sexo", "Telefone"]} doctors={handleShowDoctor()} />
+                <TableNurse />
             </section>
         </div>
     );
@@ -78,12 +64,17 @@ const mapDispatchToProps = (dispatch) => {
             const action = setDoctors(doctors);
             dispatch(action);
         },
+        populateNurses(nurses) {
+            const action = setNurses(nurses);
+            dispatch(action);
+        },
     };
 };
 
 const mapStateToProps = (states) => {
     return {
         doctors: states.doctors,
+        nurses: states.nurses,
     };
 };
 
