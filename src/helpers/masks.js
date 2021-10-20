385c319d8acb3ff642ca7e8a84aaf432f819f60d
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2021-10-20 20:40:07 -0300

    feat(Mascara): :sparkles: Implmenetação de novas mascaras de inputs

--- src/helpers/masks.js
+++ src/helpers/masks.js
@@ -7,6 +7,22 @@ export const cpfMask = (value) => {
         .replace(/(-\d{2})\d+?$/, "$1"); // captura 2 numeros seguidos de um traço e não deixa ser digitado mais nada
 };
 
+export const rgMask = (value) => {
+    return value
+        .replace(/\D/g, "") // substitui qualquer caracter que nao seja numero por nada
+        .replace(/(\d{2})(\d)/, "$1.$2") // captura 2 grupos de numero o primeiro de 3 e o segundo de 1, apos capturar o primeiro grupo ele adiciona um ponto antes do segundo grupo de numero
+        .replace(/(\d{3})(\d)/, "$1.$2")
+        .replace(/(\d{3})(\d{1,2})/, "$1-$2")
+        .replace(/(-\d{1})\d+?$/, "$1"); // captura 2 numeros seguidos de um traço e não deixa ser digitado mais nada
+};
+
+export const cepMask = (value) => {
+    return value
+        .replace(/\D/g, "") // substitui qualquer caracter que nao seja numero por nada
+        .replace(/(\d{5})(\d)/, "$1-$2") // captura 2 grupos de numero o primeiro de 3 e o segundo de 1, apos capturar o primeiro grupo ele adiciona um ponto antes do segundo grupo de numero
+        .replace(/(-\d{3})\d+?$/, "$1"); // captura 2 numeros seguidos de um traço e não deixa ser digitado mais nada
+};
+
 export const cnpjMask = (value) => {
     return value
         .replace(/\D/g, "") // substitui qualquer caracter que nao seja numero por nada
