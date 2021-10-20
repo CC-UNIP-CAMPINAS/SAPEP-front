3c4ad8096bb500c5551d7badfa66d017aaefde1c
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2021-10-20 20:39:28 -0300

    feat(Redux): :sparkles: Adição de uma entrada padrão de paciente

--- src/store/reducers/patients.reducer.js
+++ src/store/reducers/patients.reducer.js
@@ -1,6 +1,22 @@
 import types from "../types";
 
-const initialState = [];
+const initialState = [
+    {
+        id: 1,
+        name: "Leonardo", //ok
+        lastName: "Petta do Nascimento", //ok
+        cep: "130330-10", //ok
+        addressNumber: "695", //ok
+        address: "Avenida Brigadeiro Rafael Tobias de Aguiar, Jardim Aurélia, Campinas - SP",
+        complement: "Casa", //ok
+        phone: "(19) 99383-2547", //ok
+        birthday: "2000-10-09T00:00:00.000Z", //ok
+        cpf: "473.382.048-88", //ok
+        rg: "39.329.130-3", //ok
+        healthInsurance: "AMIL", //ok
+        gender: "M", //ok
+    },
+];
 
 export default function configsReducer(state = initialState, action) {
     switch (action.type) {
