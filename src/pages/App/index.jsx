2febb1a19cd179cec3970d03248281ec316dcbb1
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2021-10-21 18:33:49 -0300

    feat(Page: App): :sparkles: Aplicando a rota de usuário médico

--- src/pages/App/index.jsx
+++ src/pages/App/index.jsx
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import { Redirect, Route, Switch } from "react-router-dom";
 import NavBar from "../../components/NavBar";
 import Adm from "../Adm";
+import Doctor from "../Doctor";
 import Home from "../home/index";
 import "./styles.scoped.scss";
 
@@ -11,6 +12,8 @@ function App({ user }) {
         switch (user.groupId) {
             case 3:
                 return [<Route key="/" component={Adm} />];
+            case 1:
+                return [<Route key="/" component={Doctor} />];
             default:
                 return [<Route key="/" component={Home} />];
         }
