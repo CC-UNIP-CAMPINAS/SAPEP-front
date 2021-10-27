afa902028f407843c740a23492cbd4df0a19dc70
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2021-10-27 00:03:04 -0300

    feat(Page: Patient): :sparkles: Implementação de um botão para voltar a lista de pacientes

--- src/components/CardPatientInformation/index.jsx
+++ src/components/CardPatientInformation/index.jsx
@@ -1,24 +1,23 @@
+import { Icon } from "@iconify/react";
 import dayjs, { utc } from "dayjs";
 import React from "react";
+import { useHistory } from "react-router";
 import "./styles.scoped.scss";
 
 function CardPatientInformation({ patient }) {
     dayjs.extend(utc);
+    const history = useHistory();
 
     return (
         <section className="container">
+            <div id="button-return" onClick={() => history.goBack()}>
+                Voltar
+                <Icon icon="ph:key-return-fill" />
+            </div>
             <span>Nome: </span>
             <p>{patient.name + " " + patient.lastName}</p>
             <span>Endereço:</span>
-            <p>
-                {patient.address +
-                    ", " +
-                    patient.addressNumber +
-                    ", " +
-                    patient.cep +
-                    " Complemento: " +
-                    patient.complement}
-            </p>
+            <p>{patient.address}</p>
             <span>Telefone:</span>
             <p>{patient.phone}</p>
             <span>Data de Nascimento: </span>
