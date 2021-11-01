d7ef6cf374382567309eb3e5bef57fb03a7f88a4
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2021-11-01 18:18:04 -0300

    feat(Component: CardMedicalPrescription): ✨ implementação da visualização de execução e implementação da função de execução de uma prescrição de médica

--- src/components/CardMedicalPrescription/index.jsx
+++ src/components/CardMedicalPrescription/index.jsx
@@ -3,8 +3,36 @@ import React from "react";
 
 import "./styles.scoped.scss";
 import dayjs from "dayjs";
+import Button from "../Button/Button";
+import { connect } from "react-redux";
+import { api } from "../../services/api";
+import { useParams } from "react-router";
+import { notification } from "../../services/toastify";
+import types from "../../services/types";
+import { setRealizedMedicalPrescription } from "../../store/actions/patients.action";
 
-function CardMedicalPrescription({ prescription }) {
+function CardMedicalPrescription({ prescription, user, setRealized }) {
+    const { id } = useParams();
+
+    async function handleExecutePrescription() {
+        try {
+            const { data } = await api.patch("/medical-prescription/set-realized", { id: prescription.id });
+            setRealized({ data, patientId: id });
+            notification(types.SUCCESS, "Prescrição executada.");
+        } catch (error) {
+            console.log(error);
+            if (error.response.status === 401) {
+                notification(types.NOT_AUTHORIZED, error.response.data.message);
+            }
+            if (error.response.status === 409) {
+                notification(types.WARNING, error.response.data.message);
+            }
+            if (error.response.status === 500) {
+                notification(types.ERROR, "Algum problema ocorreu, tente novamente.");
+            }
+        }
+    }
+
     return (
         <div className="container">
             <header>
@@ -61,31 +89,44 @@ function CardMedicalPrescription({ prescription }) {
                 </div>
 
                 <div id="administration">
-                    <p>
-                        <span>Administrações:</span>
-                    </p>
+                    <main>
+                        <p>
+                            <span>Administrações:</span>
+                        </p>
 
-                    {prescription.Executors.length ? (
-                        <div>
-                            <table>
-                                <thead>
-                                    <tr>
-                                        <th>Data</th>
-                                        <th>Executor</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {prescription.Executors.map((executor, index) => (
-                                        <tr key={index}>
-                                            <td>{dayjs(executor.executionDate).format("DD/MM/YYYY [ás] HH:mm:ss")}</td>
-                                            <td>{executor.Executor.user.name}</td>
+                        {prescription.Executors.length ? (
+                            <div>
+                                <table>
+                                    <thead>
+                                        <tr>
+                                            <th>Data</th>
+                                            <th>Executor</th>
                                         </tr>
-                                    ))}
-                                </tbody>
-                            </table>
-                        </div>
+                                    </thead>
+                                    <tbody>
+                                        {prescription.Executors.map((executor, index) => (
+                                            <tr key={index}>
+                                                <td>
+                                                    {dayjs(executor.executionDate).format("DD/MM/YYYY [ás] HH:mm:ss")}
+                                                </td>
+                                                <td>{executor.Executor.user.name}</td>
+                                            </tr>
+                                        ))}
+                                    </tbody>
+                                </table>
+                            </div>
+                        ) : (
+                            <p>Nenhuma administração.</p>
+                        )}
+                    </main>
+                    {prescription.realized ? (
+                        ""
+                    ) : user.groupId === 2 ? (
+                        <span>
+                            <Button text="Executar" color="cyan" handle={handleExecutePrescription} />
+                        </span>
                     ) : (
-                        <p>Nenhuma administração.</p>
+                        ""
                     )}
                 </div>
             </section>
@@ -93,4 +134,22 @@ function CardMedicalPrescription({ prescription }) {
     );
 }
 
-export default CardMedicalPrescription;
+const mapStateToProps = (states) => {
+    return {
+        user: states.user,
+    };
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        setRealized(prescription) {
+            const action = setRealizedMedicalPrescription(prescription);
+            dispatch(action);
+        },
+    };
+};
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(CardMedicalPrescription);
