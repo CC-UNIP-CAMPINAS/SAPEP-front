529cbfebec73b05ed1185ed8e25c94644bc1fd8d
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2021-11-13 00:25:08 -0300

    feat(Login): :sparkles: Implementação do botão link para resetar a senha

--- src/components/CardLogin/index.jsx
+++ src/components/CardLogin/index.jsx
@@ -1,11 +1,41 @@
 import React from "react";
+import { api } from "../../services/api";
 import Button from "../Button/Button";
 import "./styles.scoped.scss";
+import types from "../../services/types";
+import { notification } from "../../services/toastify";
 
 function CardLogin({ inputs, setInputs, handleLogin }) {
     async function execOnKeyPress(event) {
         if (event.key === "Enter") {
             document.getElementById("button-login")?.click();
+        }
+    }
+
+    async function handleResetPassword() {
+        try {
+            if (inputs.user !== "") {
+                const { data } = await api.post(`${process.env.REACT_APP_API_HOST}/reset-password`, {
+                    email: inputs.user,
+                });
+                notification(types.SUCCESS, data.message);
+            } else {
+                notification(types.INFO, "Digite um email válido.");
+            }
+        } catch (error) {
+            if (error.response.status === 401) {
+                return notification(types.NOT_AUTHORIZED, error.response.data.message);
+            }
+            if (error.response.status === 409) {
+                return notification(types.WARNING, error.response.data.message);
+            }
+            if (error.response.status === 404) {
+                return notification(types.WARNING, error.response.data.message);
+            }
+            if (error.response.status === 400) {
+                return notification(types.WARNING, error.response.data.message);
+            }
+            notification(types.ERROR, "Algum problema ocorreu, tente novamente.");
         }
     }
 
@@ -26,7 +56,6 @@ function CardLogin({ inputs, setInputs, handleLogin }) {
                         execOnKeyPress(event);
                     }}
                 />
-
                 <label>Senha</label>
                 <input
                     value={inputs.password}
@@ -37,6 +66,12 @@ function CardLogin({ inputs, setInputs, handleLogin }) {
                         execOnKeyPress(event);
                     }}
                 />
+                <p>
+                    Esqueceu sua senha?{" "}
+                    <button className="link" onClick={handleResetPassword}>
+                        Clique aqui!
+                    </button>
+                </p>
             </div>
 
             <Button id="button-login" handle={handleLogin} text="Entrar" isLoading={true} color="cyan" disable />
